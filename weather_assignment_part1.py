937b3031c76d5d778f0d49932695575ecc7a67cd
Priyal Ranka <p.ranka@students.uu.nl>
2023-01-17 20:35:20 +0530

    final commit

--- weather_assignment_part1.py
+++ weather_assignment_part1.py
@@ -2,6 +2,7 @@ import json
 with open('precipitation.json') as file:
   measurements_dict=json.load(file)
 
+#part 1
 total_monthly_precipitation=[0,0,0,0,0,0,0,0,0,0,0,0] #a list where each element corresponds to the total precipitation value for each month
 
 for station_line in measurements_dict:
@@ -14,21 +15,38 @@ for station_line in measurements_dict:
         month=split_date[1]
         month=int(month)
         total_monthly_precipitation[month-1]+=seattle_value  #the index of the list corresponds to the (month-1)
-        print(total_monthly_precipitation)    
-    
+        
+
+
+#part 2
+total_yearly_precipitation=sum(total_monthly_precipitation)
+
+relative_monthly_precipitation_list=[] #creating an empty list
+
+for monthly_precipitation in total_monthly_precipitation: #looped so that the relative is calculated using each month's total
+   relative_monthly_precipitation=monthly_precipitation/total_yearly_precipitation
+   relative_monthly_precipitation_list.append(relative_monthly_precipitation) #adding the relative values add the end of the empty list
+print(relative_monthly_precipitation_list)
+
+
 import json
 results={
     "Seattle":{
         "State": "WA",
         "Station": "GHCND:US1WAKG0038",
-        "total_monthly_precipitation": total_monthly_precipitation
+        "total_monthly_precipitation": total_monthly_precipitation,
+        "total_yearly_precipitation": total_yearly_precipitation,
+        "relative_monthly_precipitation": relative_monthly_precipitation_list
         }
     }
 
 with open('results.json', 'w', encoding='utf-8') as file:
     json.dump(results, file, indent=4)
 
-        
+
+
+
+
 
 
 
